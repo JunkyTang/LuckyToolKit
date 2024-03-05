839d0ea514c08a93738d951f229fd997f408e184
汤俊杰 <921969987@qq.com>
2024-03-05 09:37:59 +0800

    enum

---
+++ LuckyToolKit/Classes/Codable/CodableEnumeration.swift
@@ -0,0 +1,30 @@
+//
+//  CodableEnumeration.swift
+//  LuckyToolKit
+//
+//  Created by junky on 2024/3/5.
+//
+
+import Foundation
+
+
+
+public protocol CodableEnumeration: RawRepresentable, Codable where RawValue: Codable {
+    static var unknown: Self { get }
+}
+
+public extension CodableEnumeration {
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(RawValue.self)
+        
+        if let value = Self(rawValue: rawValue) {
+            self = value
+        } else {
+            self = .unknown
+        }
+    }
+}
+
+
